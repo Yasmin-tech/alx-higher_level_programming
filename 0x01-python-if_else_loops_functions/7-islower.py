a96f5e8eda18857a4c990cd2bf7c1cf1e3c7757a
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-09-05 16:01:57 +0300

    edit 3 task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,13 +4,10 @@ def islower(c):
 
     return True if c is lowercase character and False otherwise
     """
-    try:
-        ascii_repres = ord(c)
-        if ascii_repres >= 97 and ascii_repres <= 122:
-            return True
-        else:
-            return False
-    except TypeError:
+    ascii_repres = ord(c)
+    if ascii_repres >= 97 and ascii_repres <= 122:
+        return True
+    else:
         return False
 
 
