aba3f14028553365fec825dfd54a1182fea368c8
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-09-05 17:02:40 +0300

    edit task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    """ a function that prints the last digit of a number.
+    """a function that prints the last digit of a number.
 
-        Return: Last Digit in the number
+    Return: Last Digit in the number
     """
+    if not isinstance(number, int):
+        raise TypeError("number must be an integer")
     num_str = str(number)
     print(num_str[-1], end="")
     return num_str[-1]
