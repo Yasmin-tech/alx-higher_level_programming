98172d1404556c7e88d090a5bcd45cd23aa81e4b
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-09-07 19:15:42 +0300

    add task 2, calculate command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    if argc == 1:
+        print("0 arguments.")
+    elif argc == 2:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
+    else:
+        print(f"{argc - 1} argument:")
+        for i in range(argc):
+            print(f"{i:d}: {sys.argv[i]}")
+            i += 1
