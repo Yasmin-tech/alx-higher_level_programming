4eb8037cded12ce0fb602593ae1fc9baae1511b0
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-09-26 01:17:57 +0300

    edit 4  task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,8 @@ def list_division(my_list_1, my_list_2, list_length):
         return new_list
     for i in range(list_length):
         try:
-            if isinstance(my_list_1[i], (int, float)) and isinstance(my_list_2[i], (int, float)):
+            if isinstance(my_list_1[i], (int, float)) and \
+                    isinstance(my_list_2[i], (int, float)):
                 result = my_list_1[i] / my_list_2[i]
             else:
                 print("wrong type")
