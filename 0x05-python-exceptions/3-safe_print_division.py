d3b4330321a29e70e3fea6a384893edd03c9b6f5
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-09-26 00:31:19 +0300

    add task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """ a function that divides 2 integers and prints the result.
+
+    Return: a / b otherwise: None
+    """
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
