3e56c83c9833ebfc3118a4e5eb751b761cdd58f2
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-09-26 20:21:45 +0300

    add task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" a module that represente a class Square """
+
+
+class Square:
+    """ a class Square that defines a square. """
+
+    def __init__(self, size=0) -> None:
+        """ Instantiation of class Square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ getter method for size attribute. """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter method for size attribute. """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """ a method that returns the current square area. """
+
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ prints the square in # to stdout. """
+        n = self.__size
+        for i in range(n):
+            for j in range(n):
+                print("#", end="")
+            print()
+        print()
