c52f27ef1120c1fdb2e0571bdf22af5160c53751
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-09-27 20:27:59 +0300

    edit 6 task 8

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -70,7 +70,7 @@ class Square:
         """ Returns a string representation of the square. """
         n = self.__size
         if n == 0:
-            return
+            return "\n"
         square_list = []
         for line in range(self.__position[1]):
             square_list.append(str("\n"))
