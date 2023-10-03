51f934e24ad3fd1c1a73249746a48cdd379134da
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-10-03 04:23:21 +0300

    if width or height is equal to 0, perimeter is equal to 0 task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -115,4 +115,6 @@ class Rectangle:
         ----
                 self: the object
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (2 * self.__width) + (2 * self.__height)
