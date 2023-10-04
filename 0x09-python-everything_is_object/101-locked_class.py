240f59d1435d13ec88159eb4cc9924526e929d90
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-10-04 11:46:49 +0300

    add task 101 Locked Class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" LockedClass Module
+"""
+
+
 class LockedClass:
     """ Write a class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes,
     except if the new instance attribute is called first_name"""
