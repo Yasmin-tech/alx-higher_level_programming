6aeb3291a25f928442fa2fb7ecd35103359cd72f
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-10-05 12:25:02 +0300

    add test case for NaN and missing arrgument task 0

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -5,6 +5,10 @@ print(add_integer(1, 2))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
+print(add_integer(float('inf'), float('inf')))
+
+print(add_integer())
+
 try:
     print(add_integer(4, "School"))
 except Exception as e:
@@ -13,4 +17,3 @@ try:
     print(add_integer(None))
 except Exception as e:
     print(e)
-
