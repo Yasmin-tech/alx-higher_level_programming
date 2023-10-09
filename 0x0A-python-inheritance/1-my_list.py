30c925ef00065f9552b6f649de2048f1d701ecd7
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-10-10 00:41:36 +0300

    add task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module contains one class `MyList`
+
+It inharetice from the base class `list` and defines one public method
+`print_sorted(self)` that prints the list in ascending sort
+"""
+
+
+class MyList(list):
+    """a class that inherantce from list
+
+    methods
+    -------
+
+    print_sored(self) -> void
+    Public instance method:that prints the list in ascending sort
+    """
+
+    def print_sorted(self):
+        """prints the list in ascending sort."""
+        print(sorted(self))
