e3a2dea816bc2e7d88c0d08d02549c53ce108969
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-10-10 03:41:17 +0300

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This module contains one class `BaseGeometry`
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class
+
+    methods
+    -------
+    __init__(self, size): initialize an object
+
+    area(self): Return the area of the rectangle
+    """
+
+    def __init__(self, size):
+        """initialize an object
+
+        Args
+        ----
+        size : int
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__size**2
+
+    def __str__(self):
+        """return str of the object"""
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
