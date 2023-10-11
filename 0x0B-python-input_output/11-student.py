37318149a3462984b39547d032b76be2e3d7060f
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-10-11 12:08:06 +0300

    add task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" The module of Student class
+"""
+
+
+class Student:
+    """a class Student that defines a student
+
+    Public instance attributes
+    --------------------------
+
+            first_name : str
+            last_name  : str
+            age : int
+
+            Public method def to_json(self)
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initialize an object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+
+        if type(attrs) is not list:
+            return self.__dict__
+        if not all(isinstance(item, str) for item in attrs):
+            return self.__dict__
+
+        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance:"""
+        for key, value in json.items():
+            setattr(self, key, value)
