e60fcfb2287618f156f049ccdbe14712bec3dfa7
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-10-11 10:37:15 +0300

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" The module of Student class
+"""
+
+
+class Student:
+    """a class Student that defines a student
+
+    Public instance attributes
+    --------------------------
+
+            first_name : str
+            last_name  : str
+            age : int
+
+            Public method def to_json(self)
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initialize an object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
