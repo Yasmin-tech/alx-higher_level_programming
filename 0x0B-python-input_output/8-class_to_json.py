6fecd691c65484c89122088b20db986eef0ed608
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-10-11 10:20:45 +0300

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A module that contains one function to serialize an object class
+to json string
+"""
+
+
+def class_to_json(obj):
+    """a function that returns the dictionary description
+
+    with simple data structure (list, dictionary, string,
+    integer and boolean) for JSON serialization of an object:
+    """
+    return obj.__dict__
