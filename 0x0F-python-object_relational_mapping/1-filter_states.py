9b1bb004dd7c40820c0063f139e81e0c6f690681
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-12-16 00:24:42 +0300

    modefy to search only for states begins with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -22,7 +22,7 @@ def get_states_filter():
 
     # execute the query
 
-    query = "SELECT * FROM {}.states WHERE name LIKE 'N%' ORDER BY id ASC"\
+    query = "SELECT * FROM {}.states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"\
         .format(dbname)
     cur.execute(query)
     rows = cur.fetchall()
