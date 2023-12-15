588679653b13433b2bf28c24f4b1c75064ada382
Yasmin Mahmud <jazzsalam2020@gmail.com>
2023-12-15 13:22:39 +0300

    add task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# This moudel contain a script that lists all states from
+# the database hbtn_0e_0_usa
+
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    upass = sys.argv[2]
+    dbname = sys.argv[3]
+
+    # database object connection
+    db = MySQLdb.connect(host="localhost",
+                         user=uname, passwd=upass, db=dbname, port=3306)
+
+    # create a cursor to the database
+    cur = db.cursor()
+
+    # execute the query
+
+    query = "SELECT * FROM {}.states ORDER BY id ASC".format(dbname)
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
