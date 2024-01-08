f24fab145b6459514a5f76af4449244879b70f34
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-01-08 15:13:24 +0300

    use \t

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         print("Body response:")
         content = response.read()
-        print(" - type:", type(content))
-        print(" - content:", content)
-        print(" - utf8 content:", content.decode("utf-8"))
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode("utf-8"))
