88e27ee3ed4bd6e861993856948d93b8d33dfe92
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-01-08 15:02:51 +0300

    add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This model contains the code to fetches
+    https://alx-intranet.hbtn.io/status
+    and print the type, content type and if if it is a utf encoded
+"""
+
+if __name__ == "__main__":
+    from urllib import request
+
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        print("Body response:")
+        content = response.read()
+        print("     - type:", type(content))
+        print("     - content:", content)
+        print("     - utf8 content:", content.decode("utf-8"))
