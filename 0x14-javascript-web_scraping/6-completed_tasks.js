74b1a85c960f133863d75e0bc88003b721a43f3b
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-04-24 14:39:12 +0300

    add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+/* a script that computes the number of tasks completed by user id.
+ *  API URL: https://jsonplaceholder.typicode.com/todos
+ * Only users with completed task are printed
+ * Fetch the API resource with request module
+ */
+
+const request = require('request');
+
+request('https://jsonplaceholder.typicode.com/todos', (error, response, body) => {
+  const result = {};
+  if (!error) {
+    const responseJSON = JSON.parse(body);
+    let i;
+    for (i = 0; i < responseJSON.length; i++) {
+      const userId = responseJSON[i].userId;
+      if (result[String(userId)] === undefined) {
+        result[String(userId)] = 0;
+      }
+      if (responseJSON[i].completed) {
+        result[String(userId)]++;
+      }
+    }
+  }
+  console.log(result);
+});
